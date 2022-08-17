63a6a03450a70cf11e572eecf38b37d66aa0beb2
William <williancrkw@gmail.com>
2022-08-16 21:47:55 -0300

    Atualização do projeto UsuarioRepositoryTest.java

--- minhasFinancas/src/main/java/com/br/project/minhasFinancas/service/impl/LancamentoServiceImpl.java
+++ minhasFinancas/src/main/java/com/br/project/minhasFinancas/service/impl/LancamentoServiceImpl.java
@@ -6,8 +6,6 @@ import com.br.project.minhasFinancas.model.enums.StatusLancamento;
 import com.br.project.minhasFinancas.model.enums.TipoLancamento;
 import com.br.project.minhasFinancas.model.repository.LancamentoRepository;
 import com.br.project.minhasFinancas.service.LancamentoService;
-import org.hibernate.criterion.Example;
-import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -52,16 +50,6 @@ public class LancamentoServiceImpl implements LancamentoService {
         return null;
     }
 
-    /*@Override
-    @Transactional(readOnly = true)
-    public List<Lancamento> buscar(Lancamento lancamentoFiltro) {
-        Example example = Example.of( lancamentoFiltro,
-                ExampleMatcher.matching()
-                        .withIgnoreCase()
-                        .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING) );
-
-        return repository.findAll(example);
-    }*/
 
     @Override
     public void atualizarStatus(Lancamento lancamento, StatusLancamento status) {
--- minhasFinancas/src/test/java/com/br/project/minhasFinancas/model/repository/UsuarioRepositoryTest.java
+++ minhasFinancas/src/test/java/com/br/project/minhasFinancas/model/repository/UsuarioRepositoryTest.java
@@ -5,26 +5,32 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+import javax.persistence.EntityManager;
+import java.util.Optional;
+
+
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UsuarioRepositoryTest {
 
     @Autowired
     UsuarioRepository repository;
 
+    @Autowired
+    EntityManager entityManager;
+
     @Test
     public void deveVerificarAExistenciaDeUmEmail() {
         // cenario
-        Usuario usuario = Usuario.builder()
-                .nome("usuario")
-                .email("usuario@email.com")
-                .build();
-        repository.save(usuario);
+        Usuario usuario = criarUsuario();
+        entityManager.persist(usuario);
 
         //acão / execução
 
@@ -38,13 +44,61 @@ public class UsuarioRepositoryTest {
     @Test
     public void deveRetornarFalsoQuandoNaoHouverUsuarioCadastradoComOEmail() {
         //cenario
-        repository.deleteAll();
+
 
         //ação
         boolean result = repository.existsByEmail("usuario@email.com");
 
         //verificar
         Assertions.assertThat(result).isFalse();
+    }
+
+    @Test
+    public void devePersistirUmUsuarioNaBaseDeDados() {
+
+        // cenario
+        Usuario usuario = criarUsuario();
+
+        // ação
+
+        Usuario usuarioSalvo = repository.save(usuario);
+
+        // verificação
+
+        Assertions.assertThat(usuarioSalvo.getId()).isNotNull();
+
+    }
+
+    @Test
+    public void deveBuscarUmUsuarioPorEmail() {
+        // cenario
+        Usuario usuario = criarUsuario();
+        entityManager.persist(usuario);
+
+        //verificação
+        Optional<Usuario> result = repository.findByEmail("usuario@email.com");
+
+        Assertions.assertThat(result.isPresent()).isTrue();
+    }
+
+    @Test
+    public void deveRetornarVazioAoBuscarUsuarioPorEmailQuandoNaoExisteNaBase() {
+        // cenario
+
+
+        //verificação
+        Optional<Usuario> result = repository.findByEmail("usuario@email.com");
+
+        Assertions.assertThat(result.isPresent()).isFalse();
+    }
+
+    public static Usuario criarUsuario() {
+        return Usuario
+                .builder()
+                .nome("usuario")
+                .email("usuario@email.com")
+                .senha("senha")
+                .build();
     }
 
 }
