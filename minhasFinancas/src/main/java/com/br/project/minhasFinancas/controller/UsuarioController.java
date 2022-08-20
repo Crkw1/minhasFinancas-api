89b462130e084304506412b09773ee8b34a14567
William <williancrkw@gmail.com>
2022-08-20 16:59:09 -0300

    Atualização do projeto UsuarioController

---
+++ minhasFinancas/src/main/java/com/br/project/minhasFinancas/controller/UsuarioController.java
@@ -0,0 +1,37 @@
+package com.br.project.minhasFinancas.controller;
+
+import com.br.project.minhasFinancas.dto.UsuarioDTO;
+import com.br.project.minhasFinancas.exception.RegraNegocioException;
+import com.br.project.minhasFinancas.model.entity.Usuario;
+import com.br.project.minhasFinancas.service.UsuarioService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/usuarios")
+public class UsuarioController {
+
+    private UsuarioService service;
+
+    public UsuarioController ( UsuarioService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity salvar(@RequestBody UsuarioDTO dto) {
+        Usuario usuario = Usuario
+                .builder()
+                .nome(dto.getNome())
+                .email(dto.getEmail())
+                .senha(dto.getSenha())
+                .build();
+        try {
+            Usuario usuarioSalvo = service.salvarUsuario(usuario);
+            return new ResponseEntity(usuarioSalvo, HttpStatus.CREATED);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+}
