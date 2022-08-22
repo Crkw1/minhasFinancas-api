575551c01cb4b62b42aee1307ddb5ab088a0c51d
William <williancrkw@gmail.com>
2022-08-21 22:03:40 -0300

    Atualização do projeto LancamentoController

--- minhasFinancas/src/main/java/com/br/project/minhasFinancas/controller/LancamentoController.java
+++ minhasFinancas/src/main/java/com/br/project/minhasFinancas/controller/LancamentoController.java
@@ -8,20 +8,21 @@ import com.br.project.minhasFinancas.model.enums.StatusLancamento;
 import com.br.project.minhasFinancas.model.enums.TipoLancamento;
 import com.br.project.minhasFinancas.service.LancamentoService;
 import com.br.project.minhasFinancas.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api/lancamentos")
+@RequiredArgsConstructor
 public class LancamentoController {
 
-    private LancamentoService service;
-    private UsuarioService usuarioService;
-
-    public LancamentoController(LancamentoService service) {
-        this.service = service;
-    }
+    private final LancamentoService service;
+    private final UsuarioService usuarioService;
 
     @PostMapping
     public ResponseEntity salvar(@RequestBody LancamentoDTO dto) {
@@ -34,6 +35,28 @@ public class LancamentoController {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
+
+    public ResponseEntity buscar(
+            @RequestParam(value = "descricao", required = false) String descricao,
+            @RequestParam(value = "mes", required = false) Integer mes,
+            @RequestParam(value = "ano", required = false) Integer ano,
+            @RequestParam("usuario") Long idUsuario) {
+        Lancamento lancamentoFiltro = new Lancamento();
+        lancamentoFiltro.setDescricao(descricao);
+        lancamentoFiltro.setMes(mes);
+        lancamentoFiltro.setAno(ano);
+
+       Optional<Usuario> usuario = usuarioService.obterPorId(idUsuario);
+       if (usuario.isPresent()) {
+           return ResponseEntity.badRequest().body("Não foi possível realizar a consulta. Usuário não encontrado");
+       } else {
+           lancamentoFiltro.setUsuario(usuario.get());
+       }
+        List<Lancamento> lancamentos = service.buscar(lancamentoFiltro);
+       return ResponseEntity.ok(lancamentos);
+
+    }
+
 
     @DeleteMapping
     public ResponseEntity deletar(@PathVariable("id") Long id) {
@@ -72,9 +95,14 @@ public class LancamentoController {
        Usuario usuario = usuarioService.obterPorId(dto.getUsuario()).
                 orElseThrow( () -> new RegraNegocioException("Usuário não encontrado por Id informado."));
         lancamento.setUsuario(usuario);
-        lancamento.setTipo(TipoLancamento.valueOf(dto.getTipo()));
-        lancamento.setStatus(StatusLancamento.valueOf(dto.getStatus()));
 
+        if(dto.getTipo() != null) {
+            lancamento.setTipo(TipoLancamento.valueOf(dto.getTipo()));
+        }
+
+        if(dto.getStatus() != null) {
+            lancamento.setStatus(StatusLancamento.valueOf(dto.getStatus()));
+        }
         return lancamento;
     }
 }
