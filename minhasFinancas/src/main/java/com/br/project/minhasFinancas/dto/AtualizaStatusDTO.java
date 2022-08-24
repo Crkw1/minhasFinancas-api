fd60ef6d8a01a4db2193c320c8c6d9a2551db648
William <williancrkw@gmail.com>
2022-08-23 23:08:58 -0300

    Atualização do projeto AtualizaStatusDTO

---
+++ minhasFinancas/src/main/java/com/br/project/minhasFinancas/dto/AtualizaStatusDTO.java
@@ -0,0 +1,13 @@
+package com.br.project.minhasFinancas.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AtualizaStatusDTO {
+    private String status;
+}
