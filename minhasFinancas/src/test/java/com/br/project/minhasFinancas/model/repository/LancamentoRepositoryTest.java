a5cc8a8f6bc20fa3a0180775bdf734251148141c
William <williancrkw@gmail.com>
2022-08-27 09:59:41 -0300

    Testes do projeto LancamentoServiceTest.java

--- minhasFinancas/src/test/java/com/br/project/minhasFinancas/model/repository/LancamentoRepositoryTest.java
+++ minhasFinancas/src/test/java/com/br/project/minhasFinancas/model/repository/LancamentoRepositoryTest.java
@@ -6,7 +6,6 @@ import com.br.project.minhasFinancas.model.enums.TipoLancamento;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
